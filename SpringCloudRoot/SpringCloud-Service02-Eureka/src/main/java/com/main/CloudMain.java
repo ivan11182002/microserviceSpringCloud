2033c044b615b2cc590f04ec179e08ae8b7400c8
IvanKao <ivan@readycom.com.tw>
2020-01-12 17:56:32 +0800

    spring cloud v1.0

---
+++ SpringCloudRoot/SpringCloud-Service02-Eureka/src/main/java/com/main/CloudMain.java
@@ -0,0 +1,11 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMain {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudMain.class, args);
+	}
+}
